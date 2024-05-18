fd8a2268ba37bfea1c4bb8f2a7f4d2da9cb5423d
Ayushi <rajaayushi1234@gmail.com>
2024-05-18 12:22:49 +0530

    regsitration api done

--- app/api/registration/route.ts
+++ app/api/registration/route.ts
@@ -1,34 +1,37 @@
-import mssqlconnect from "@/lib/mssqlconnect";
-import { NextRequest, NextResponse } from "next/server";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import bcrypt from 'bcryptjs';
+import { db } from '@/lib/db';
 
-const sql = require('mssql')
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const { firstName, lastName, email, password, confirmPassword } = req.body;
 
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords do not match' });
+    }
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
-  try {
-    // Connect to the MongoDB database
-    //await connectMongoDb();
-    await mssqlconnect();
-    //Fetch all the countries data
-    const result = await sql.query`SELECT * FROM Country_Master ORDER BY COUNTRY_NAME ASC`;
-    // Map the result to a JSON format
-    const countryData = result.recordset.map((record: any) => ({
-      id: record.Country_Id,
-      country_name: record.COUNTRY_NAME,
-      country_flag_location: record.COUNTRY_FLAG_LOCATION,
-      country_map_location: record.COUNTRY_MAP_LOCATION,
-      country_added_by: record.COUNTRY_ADDED_BY,
-      COUNTRY_ADDED_ON: record.COUNTRY_ADDED_ON,
-      country_updated_by: record.COUNTRY_UPDATED_BY,
-      COUNTRY_UPDATED_ON: record.COUNTRY_UPDATED_ON
-      // Add more fields as needed
-    }));
+    try {
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    //console.log(result);
+      // Create the user
+      const user = await db.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword,
+        },
+      });
 
-    return new NextResponse(JSON.stringify(countryData), { status: 200, headers: { 'Content-Type': 'application/json' } });
-  } catch (error) {
-    // Handle errors and send an error response with status code 500
-    return new NextResponse("IN Api Calling error internal Error", { status: 500 });
+      res.status(201).json(user);
+    } catch (error) {
+      
+       
+        res.status(500).json({ error: 'Internal server error' });
+        
+    }
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
   }
-};
+}
