c23d2faf730b0f910ed4f8a4348d777b0318d01e
Ayushi <rajaayushi1234@gmail.com>
2024-05-18 12:58:56 +0530

    fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ import RegisterForm from '@/components/RegisterForm';
 
 export default function Register() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-500 to-purple-600 relative py-12 px-4 sm:px-6 lg:px-8">
+      
+      <div className="item-center z-10 max-w-md w-full space-y-8 bg-white bg-opacity-90 p-10 rounded-lg shadow-lg">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Register a new account
         </h2>
@@ -12,4 +13,3 @@ export default function Register() {
     </div>
   );
 }
-
